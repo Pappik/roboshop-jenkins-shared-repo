d7654a022a1885c00c545135cc217f1038003b74
pappik <pappik@gmail.com>
2023-12-27 16:25:46 -0500

    jenkins

--- vars/common.groovy
+++ vars/common.groovy
@@ -4,7 +4,7 @@ def compile() {
     }
 
     if (app_lang == "maven" ) {
-        sh 'mvn package'
+        sh 'mvn package && cp target/${component}-1.0.jar ${component}.jar'
     }
 }
 
@@ -28,8 +28,18 @@ def email(email_note) {
 
 def artifact_Push() {
     sh "echo ${TAG_NAME} >VERSION"
+
     if (app_lang == "nodejs") {
        sh "zip -r  ${component}-${TAG_NAME}.zip nodule_modules server.js VERSION ${extrafiles}"
     }
+
+    if (app_lang == "nginx" || app_lang == "python") {
+        sh "zip -r  ${component}-${TAG_NAME}.zip * -x Jenkinsfile ${extrafiles}"
+    }
+
+    if (app_lang == "maven") {
+        sh "zip -r  ${component}-${TAG_NAME}.zip ${component}.jar VERSION ${extrafiles}"
+    }
+
     sh "curl -v -u admin:admin123 --upload-file ${component}-${TAG_NAME}.zip http://172.31.88.150:8081/repository/${component}/${component}-${TAG_NAME}.zip"
 }
